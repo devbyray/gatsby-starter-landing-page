6ebf4aee7ad017b671f0fe649516ac41c09172c4
Raymon Schouwenaar <rschouwenaar@mbpvanraymon2.lan>
2019-06-28 22:14:42 +0200

    Add form

--- src/components/cta.js
+++ src/components/cta.js
@@ -9,6 +9,36 @@ const CallToAction = () => (
       title="Stay Updated"
       description="Give a final call to action because that's what the cool kids are doing."
     />
+
+    <form
+      mname="contact"
+      method="post"
+      action="/"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <div className="field half first">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+      </div>
+      <div className="field half">
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+      </div>
+      <div className="field">
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message" rows="6" />
+      </div>
+      <ul className="actions">
+        <li>
+          <input type="submit" value="Send Message" className="special" />
+        </li>
+        <li>
+          <input type="reset" value="Clear" />
+        </li>
+      </ul>
+    </form>
+
     <Button>Get Early Access</Button>
   </div>
 )
