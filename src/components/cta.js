caeb5707046baf4bc105e861fdcedd9001ee0efa
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2019-06-29 16:26:42 +0200

    Added new form

--- src/components/cta.js
+++ src/components/cta.js
@@ -9,40 +9,6 @@ const CallToAction = () => (
       title="Stay Updated"
       description="Give a final call to action because that's what the cool kids are doing."
     />
-
-    <form
-      mname="contact"
-      method="post"
-      netlify
-      action="/success"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      data-netlify-recaptcha="true"
-    >
-      <input type="hidden" name="form-name" value="contact" />
-      <div className="field half first">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-      </div>
-      <div className="field half">
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
-      </div>
-      <div className="field">
-        <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows="6" />
-      </div>
-      <div data-netlify-recaptcha="true"></div>
-      <ul className="actions">
-        <li>
-          <input type="submit" value="Send Message" className="special" />
-        </li>
-        <li>
-          <input type="reset" value="Clear" />
-        </li>
-      </ul>
-    </form>
-
     <Button>Get Early Access</Button>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import SEO from "../components/seo"
 import Hero from "../components/hero"
 import Content from "../components/content"
 import CallToAction from "../components/cta"
+import Contact from "../components/form"
 
 const IndexPage = () => (
   <Layout>
     <SEO />
     <Hero />
     <Content />
+    <Contact />
     <CallToAction />
   </Layout>
 )
